8f60c8d38e288e9ead21f0493e624bc7d39456bb
OstapPankevych <ostappankevych@gmail.com>
2015-05-19 21:23:41 +0300

    phase #2

---
+++ BattleShip.GameEngine.Test/GameEventArgs/GameEvenArgsTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BattleShip.GameEngine.GameEvent;
+using BattleShip.GameEngine.GameEventArgs;
+using BattleShip.GameEngine.Location;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BattleShip.GameEngine.Test.GameEventArgs
+{
+    [TestClass]
+    public class GameEvenArgsTest
+    {
+        [TestMethod]
+        public void Constr()
+        {
+            Assert.IsTrue((new GameEvenArgs(new Position(5, 5))).Location == new Position(5, 5));
+        }
+    }
+}
---
+++ BattleShip.GameEngine.Test/GameEventArgs/ProtectEventArgsTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BattleShip.GameEngine.Fields.Cells.StatusCell;
+using BattleShip.GameEngine.GameEventArgs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BattleShip.GameEngine.Test.GameEventArgs
+{
+    [TestClass]
+    public class ProtectEventArgsTest
+    {
+        [TestMethod]
+        public void Constr()
+        {
+            Assert.IsTrue((new ProtectEventArgs(typeof(EmptyCell))).Type == typeof(EmptyCell));
+        }
+    }
+}
