4107bccfaa82f0317999eb3ce4cee8b8b5796969
OstapPankevych <ostappankevych@gmail.com>
2015-05-19 03:35:01 +0300

    phase #2

---
+++ BattleShip.DesktopUI/Core/GameSettings.cs
@@ -0,0 +1,35 @@
+﻿using BattleShip.GameEngine.Game.GameModes;
+
+namespace BattleShip.DesktopUI.Core
+{
+    internal enum PlayerVS
+    {
+        Player,
+        Computer
+    };
+
+    struct GameSettings
+    {
+        public IGameMode GameMode { get; private set; }
+        public PlayerVS PlayerVs { get; private set; }
+
+        public bool PlayerVSPlayer()
+        {
+            if (PlayerVs == PlayerVS.Player)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public void SetPlayerVS(PlayerVS playerVS)
+        {
+            PlayerVs = playerVS;
+        }
+
+        public void SetGameMode(IGameMode gameMode)
+        {
+            GameMode = gameMode;
+        }
+    }
+}
